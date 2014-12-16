2376854501e2459c042744ff6c4165c952f7fb09
ThanhMinh <thanhminh92it@gmail.com>
2014-12-17 00:50:47 +0700

    - Tạo trang exam
    - Bắt đầu code

---
+++ EUTmvc/Controllers/ExamController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EUTmvc.Controllers
+{
+    public class ExamController : Controller
+    {
+        //
+        // GET: /Exam/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ExamEnglish()
+        {
+            return View();
+        }
+    }
+}
